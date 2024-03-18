530325bc6d9a067f2286126b6bfeb60683301af3
ken55ty <baskent.5.23@gmail.com>
2024-03-18 10:30:09 +0900

    add: マイグレーションファイル編集

--- db/migrate/20240318011054_sorcery_core.rb
+++ db/migrate/20240318011054_sorcery_core.rb
@@ -1,9 +1,11 @@
 class SorceryCore < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
+      t.string :name,             null: false
       t.string :email,            null: false, index: { unique: true }
       t.string :crypted_password
       t.string :salt
+      t.text :profile
 
       t.timestamps                null: false
     end
