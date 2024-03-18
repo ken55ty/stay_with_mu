f01b9b8c58d52106ac123e4d1d0b66fda2929aef
ken55ty <baskent.5.23@gmail.com>
2024-03-18 17:02:05 +0900

    add: login用のビューを作成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,8 @@
 class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
+
+  def new; end
+
   def create
     @user = login(params[:email], params[:password])
 
