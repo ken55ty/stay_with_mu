cbc538ca48a5b87d5f519ec70f26baad4ea53bab
ken55ty <baskent.5.23@gmail.com>
2024-03-18 15:48:34 +0900

    add: user登録のビュー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+
 
   def new
     @user = User.new
@@ -12,11 +12,12 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
