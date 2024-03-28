933df18bd87d9243600ce8dc03f99d6e0445abf7
ken55ty <baskent.5.23@gmail.com>
2024-03-28 17:00:32 +0900

    add: メモリーが更新された時にexpを更新するコールバックメソッドを作成

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -6,6 +6,7 @@ class Memory < ApplicationRecord
   validates :body, presence: true, length: { maximum: 65_535 }
   validate :validate_tag_count
 
+  after_commit :update_music_exp
   private
 
   def validate_tag_count
@@ -13,5 +14,14 @@ class Memory < ApplicationRecord
     if tag_ids.count > max_tags
       errors.add(:base, "選択できるタグは#{max_tags}個までです")
     end
+  end
+
+  private
+
+  def update_music_exp
+    # musicに紐づくすべてのmemoryのbodyの文字数を合計
+    total_exp = self.music.memories.sum { |memory| memory.body.length }
+    # 合計値をmusicのexpとして保存
+    self.music.update(experience_point: total_exp)
   end
 end
