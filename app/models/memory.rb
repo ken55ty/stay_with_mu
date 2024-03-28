808f2ca165b579916611b4259f965ea5ef54cf2c
ken55ty <baskent.5.23@gmail.com>
2024-03-28 11:01:09 +0900

    add: タグの選択をを3つまでに制限するバリデーションを追加

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -4,4 +4,14 @@ class Memory < ApplicationRecord
   has_many :tags, through: :memory_tags
 
   validates :body, presence: true, length: { maximum: 65_535 }
+  validate :validate_tag_count
+
+  private
+
+  def validate_tag_count
+    max_tags = 3
+    if tag_ids.count > max_tags
+      errors.add(:base, "選択できるタグは#{max_tags}個までです")
+    end
+  end
 end
