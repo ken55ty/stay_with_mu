95995158bcc03dae8b0a4eb5f6b3987915081769
ken55ty <baskent.5.23@gmail.com>
2024-03-29 23:15:57 +0900

    add: usersコントローラーに@musicsを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @musics = @user.musics.includes(memories: :tags).order(updated_at: :desc).page(params[:page])
   end
 
   def edit
