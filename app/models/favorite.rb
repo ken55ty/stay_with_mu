fe9d90f071d3c33352b78f95b106b1db25c2afff
ken55ty <baskent.5.23@gmail.com>
2024-04-23 16:00:38 +0900

    add: いいね時にMUユーザーに通知を送る

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -4,4 +4,18 @@ class Favorite < ApplicationRecord
   has_one :notification, as: :notifiable, dependent: :destroy
   counter_culture :music
   validates :user_id, presence: true, uniqueness: { scope: :music_id }
+
+  after_create_commit :create_favorite_notification
+
+  private
+
+  def create_favorite_notification
+    return if self.user_id == self.music.user_id
+
+    Notification.create(
+      sender_id: self.user_id,
+      recipient_id: self.music.user_id,
+      notifiable: self
+    )
+  end
 end
