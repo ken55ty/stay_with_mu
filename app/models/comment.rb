582f9143167f81d6b2b98ff7d79f95cfc5ec0272
ken55ty <baskent.5.23@gmail.com>
2024-04-23 14:14:40 +0900

    add: Notificationモデル追加、各モデルのアソシエーション追加。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :music, touch: true
+  has_one :notification, as: :notifiable, dependent: :destroy
   counter_culture :music
   validates :body, presence: true, length: { maximum: 65_535 }
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :music
+  has_one :notification, as: :notifiable, dependent: :destroy
   counter_culture :music
   validates :user_id, presence: true, uniqueness: { scope: :music_id }
 end
