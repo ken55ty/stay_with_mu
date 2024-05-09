ec6bbe2a141fd7759666b70f3c11096cbe44226a
ken55ty <baskent.5.23@gmail.com>
2024-05-09 13:59:22 +0900

    add: seedファイルにお題を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,20 @@ prev_threshold = 0
 # 各レベルの閾値を設定
 (2..100).each do |level|
   # 次のレベルに必要なexpを1ずつ上げる
+  #レベル1からレベル2までの閾値を5にするために2+3の3を足している
   threshold = prev_threshold + level + 3
   LevelSetting.find_or_create_by!(level:, threshold:)
   prev_threshold = threshold
+end
+
+topics = {
+  1 => '一番最初にハマったアーティストの話',
+  2 => 'ランニングの時に聴きたい曲',
+  3 => '夜に聴きたくなる曲',
+  4 => '初めて自分で買ったCD',
+  5 => '印象に残っているライブ'
+}
+
+topics.each do |id, topic|
+  RecommendedTopic.find_or_create_by!(id:, topic:)
 end
