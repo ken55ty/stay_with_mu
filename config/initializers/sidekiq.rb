3f706ba8f59ca96cd6d939dd73fcff23453fd34a
ken55ty <baskent.5.23@gmail.com>
2024-05-10 18:42:00 +0900

    edit: 毎日0時にお題を更新するように戻す

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,16 +2,16 @@ require 'sidekiq'
 require 'sidekiq-cron'
 
 Sidekiq.configure_server do |config|
-    schedule_file = 'config/schedule.yaml'
-    config.redis = {
-      url: ENV['REDIS_URL']
-    }
+  schedule_file = 'config/schedule.yaml'
+  config.redis = {
+    url: ENV['REDIS_URL']
+  }
 
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
 end
 
-  Sidekiq.configure_client do |config|
-    config.redis = {
-      url: ENV['REDIS_URL']
-    }
-  end
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV['REDIS_URL']
+  }
+end
